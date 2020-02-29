f85c017a28f99c5b88a18fb059463a141754e9ba
MauroMile <50807976+MauroMile@users.noreply.github.com>
2020-02-29 12:12:24 +0100

    Update dijkstra.cpp

--- Problemi/Grafi/dijkstra.cpp
+++ Problemi/Grafi/dijkstra.cpp
@@ -16,18 +16,25 @@ ull *dist;
 int N,M,start,fine;
 
 void dijkstra(){
-	for(int i=0;i<N;i++) dist[i]=1ULL<<60;		//Inserisco dei valori giganti nelle distanze (infinito)
-	priority_queue <li, vector<li>, greater<li> > Q; //inizializzazione della priority queue
-	dist[start]=0;									//Metto a 0 la distanza del nodo di partenza
-	Q.push(make_pair(0,start));						//Faccio cominciare il ciclo inserendo il nodo di partenza
-	while(!Q.empty()){
-		ull d=Q.top().first;						//tolgo il primo elemento della coda 
+	for(int i=0;i<N;i++) dist[i]=1ULL<<60;				//Inserisco dei valori giganti nelle distanze (infinito)
+	priority_queue <li, vector<li>, greater<li> > Q; 	//inizializzazione della priority queue
+	dist[start]=0;										//Metto a 0 la distanza del nodo di partenza
+	Q.push(make_pair(0,start));							//Faccio cominciare il ciclo inserendo il nodo di partenza
+	while(!Q.empty())
+	{
+		ull d=Q.top().first;							//tolgo il primo elemento della coda 
 		int n=Q.top().second;
 		Q.pop();
-		for(int i=0;i<G[n].size();i++){									//guardo intorno al nodo e controllo tutti i nodi che posso raggiungere
-			if(dist[G[n][i].first]>d+G[n][i].second){				//se il nodo lo posso raggiungere con meno strada di quella che ho gia` fatto
-				dist[G[n][i].first]=d+G[n][i].second;				//aggiorno la sua distanza e lo inserisco nella coda come candidato per il
-				Q.push(make_pair(dist[G[n][i].first],G[n][i].first));	//percorso piu` breve
+		
+		//guardo intorno al nodo e controllo tutti i nodi che posso raggiungere
+		for(int i=0;i<G[n].size();i++)
+		{							
+			//se il nodo lo posso raggiungere con meno strada di quella che ho gia` fatto
+			if(dist[G[n][i].first]>d+G[n][i].second)
+			{
+				//aggiorno la sua distanza e lo inserisco nella coda come candidato per il percorso più breve
+				dist[G[n][i].first]=d+G[n][i].second;			
+				Q.push(make_pair(dist[G[n][i].first],G[n][i].first));	
 			}
 		}
     }	
@@ -37,12 +44,15 @@ void dijkstra(){
  	fin>>N>>M>>start>>fine;
  	G.resize(N);
 	dist= new ull [N];
-	start--;						//interi 1-based (quindi devo abbassarli di 1)
+	start--;						//nel problema i numeri partono da 1 ma io li faccio partire da 0
 	fine--;
-	for(int i=0;i<M;i++){			//caricamento grafo
+	 
+	 //caricamento grafo
+	for(int i=0;i<M;i++)
+	{			
 		int a,b,w;
 		fin>>a>>b>>w;
-		a--;						//interi 1-based (quindi devo abbassarli di 1)
+		a--;						//nel problema i numeri partono da 1 ma io li faccio partire da 0
 		b--;
 		G[a].push_back(make_pair(b,w));
 	}
